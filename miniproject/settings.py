acbb1a6f4948edf568dec60f0c9984572f3199a5
rina <rinudiya45@gmail.com>
2025-03-21 12:11:57 +0530

    fourth commit

--- miniproject/settings.py
+++ miniproject/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p57*=ilsjm3+l60y@)e#k3(&de97lm+_#!n))6v&@#_c%owb)y'
+SECRET_KEY = '5=koir8dxl!8#y#^bcj-3)slq0qg!h+z!zvs2g)ox*8tis!^_y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR,"staticfiles")
+STATICFILES_DIRS=[os.path.join(BASE_DIR,"static")]
 MEDIA_URL ='/image/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media/')
 
